8f531e3b22e7f3af09e21cd9c3abaa7f50c03caf
Jamoladdin <jamoladdinisnatdinov2@gmail.com>
2024-11-29 00:17:06 +0500

    little updatecha

--- src/components/Features/SingleFeature.tsx
+++ src/components/Features/SingleFeature.tsx
@@ -10,10 +10,10 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
             {icon}
           </div>
         </div>
-        <h3 className="mb-5 text-center sm:text-start text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+        <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
           {title}
         </h3>
-        <p className="pr-[10px] text-center sm:text-start text-base font-medium leading-relaxed text-body-color">
+        <p className="pr-[10px] text-base font-medium leading-relaxed text-body-color">
           {paragraph}
         </p>
       </div>
